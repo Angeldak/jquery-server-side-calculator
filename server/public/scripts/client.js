5d0477c9da0d41ae8e6aad2ea491e9ecd32692bc
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-09 16:42:35 -0500

    html and css from prior project added for similar look, also, added client function shells

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,3 +4,26 @@ function onReady() {
 
 }
 
+function appendData(){
+
+}
+
+function collectData() {
+
+}
+
+function checkOperator(keyStroke) {
+
+}
+
+function getRequest(path) {
+
+}
+
+function postRequest(path, data) {
+
+}
+
+function checkError(error){
+    
+}
