7874cf520de7c00eb2c4fccb0676966bfaef3a4e
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-09 18:07:19 -0500

    working array to object math

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,5 @@
 $(onReady);
+const allowedOperators = ["+", "-", "*", "/"];
 let currentDisplay = "";
 
 function onReady() {
@@ -32,6 +33,47 @@ function displayUpdate(event) {
 
 function getRequest(path) {
 
+}
+
+function mathArrayToObject(array) {
+    let operandToggle = true;
+    const newObject = {
+        firstOperand: "",
+        secondOperand: "",
+        operator: ""
+    };
+    let newData = array.map((item) => item);
+    for (const info of newData) {
+        if (!isNaN(Number(info)) && operandToggle === true) {
+            console.log(newObject.firstOperand, info);
+            newObject.firstOperand += info;
+        } else if (allowedOperators.includes(info) && operandToggle === true) {
+            newObject.operator = info;
+            operandToggle = false;
+        } else if (allowedOperators.includes(info)) {
+            switch (newObject.operator) {
+                case "+":
+                    newObject.firstOperand = (Number(newObject.firstOperand) + Number(newObject.secondOperand)).toString();
+                    break;
+                case "-":
+                    newObject.firstOperand = (Number(newObject.firstOperand) - Number(newObject.secondOperand)).toString();
+                    break;
+                case "*":
+                    newObject.firstOperand = (Number(newObject.firstOperand) * Number(newObject.secondOperand)).toString();
+                    break;
+                case "/":
+                    newObject.firstOperand = (Number(newObject.firstOperand) / Number(newObject.secondOperand)).toString();
+                    break;
+                default:
+                    console.log("Error not an operator!");
+            }
+            newObject.operator = info;
+            newObject.secondOperand = "";
+        } else {
+            newObject.secondOperand += info;
+        }
+    }
+    return newObject;
 }
 
 function postRequest(path, data) {
