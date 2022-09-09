a5eedd18ade1c3fbbcd9f43425b5e203d184394f
Lloyd Prock <lloyd.prock@gmail.com>
2022-09-09 15:49:35 -0500

    working GET and POST for math

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
+const mathWithTotals = [];
 
 app.use(express.static("server/public"));
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/math", (req, res) => {
+    res.send(mathWithTotals);
+});
+
+app.post("/math", (req, res) => {
+    mathWithTotals.push(req.body);
+    res.status(201).send("Received math data!");
+    console.log("Math data received:", req.body);
+});
 
 app.listen(PORT, () => {
     console.log("Listening on Port: 5000");
